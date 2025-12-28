6233827443c30e0a2af7f1bbd6fad8f49df6d23f
SpyerNot <lowkimsenglowkimseng8@gmail.com>
2025-12-28 22:18:51 +0800

    Implement cost deduction for S1 and S3
    Added functionality to deduct costs from S1 and S3 total.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,17 @@ import streamlit as st
 st.title("Bookstore calculator")
 st.caption("Used to calculate all the stationaries without using an actual calculator")
 option = st.selectbox("What level is the buyer studying?", ("S1","S2","S3","S4","S5"),)
-s1 = s3 = 138
+s1 = 138
 s2 = 134.6
 s4e = 119.3
 s4na = 115.5
 s4nt = 118.7
+while True:
+  if option == "S1" or option == "S3":
+    st.write("The total cost of the stationaries is $138")
+    remove = st.number_input("Enter the cost that you want to deduct off $138: ")
+    if remove == 0:
+      break
+      st.write(s1)
+    else:
+      s1 -= remove
